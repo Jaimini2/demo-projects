7946f1adc9f12f4f7f4584e90a268f3d4e964090
jaimini2 <mehtajaimini2@gmail.com>
2025-04-07 23:56:24 +0530

    "First commit for third party client demo "

---
+++ thirdpartyClientDemo/src/main/java/com/example/thirdpartyClientDemo/ThirdpartyClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdpartyClientDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdpartyClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdpartyClientDemoApplication.class, args);
+	}
+
+}
